ca79574ba72801cd3d73c31d3a365c4c07d5fcfb
CalebScott0 <calebbenscott@gmail.com>
2024-08-16 15:40:33 -0500

    adding image carousel component

--- project/src/components/Carousel.jsx
+++ project/src/components/Carousel.jsx
@@ -10,7 +10,7 @@ import Autoplay from "embla-carousel-autoplay";
 export const CarouselTemplate = ({ imageArr }) => {
   return (
     <Carousel
-    className = "w-full"
+      className="w-full"
       opts={{
         loop: true,
       }}
@@ -23,11 +23,9 @@ export const CarouselTemplate = ({ imageArr }) => {
       <CarouselContent className="">
         {imageArr.map((image, index) => (
           <CarouselItem key={index}>
-            <div className="p-1">
-              <CarouselContent className="flex aspect-square">
-                <img className="max-h-[600px] w-full object-fill" src={image.src} alt={image.alt} />
-              </CarouselContent>
-            </div>
+            <CarouselContent className="flex aspect-square">
+              <img src={image.src} alt={image.alt} />
+            </CarouselContent>
           </CarouselItem>
         ))}
       </CarouselContent>
