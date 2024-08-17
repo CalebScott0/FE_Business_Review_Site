bed4eee4aba7f5dcaed45f9de7658bb35e86655d
CalebScott0 <calebbenscott@gmail.com>
2024-08-17 01:23:11 -0500

    category search bar up and running

--- project/src/features/navbar/NavBar.jsx
+++ project/src/features/navbar/NavBar.jsx
@@ -5,6 +5,7 @@ import { useGetCategoriesQuery } from "./categoryslice";
 import SearchCategories from "./SearchCategories";
 const NavBar = () => {
   const { data = {}, error, isLoading } = useGetCategoriesQuery();
+  // TEST SPEED OF THIS WHEN MOVED TO SEARCHCATEGORY COMPONENT WITH NEW PRISMA QUERY
   let categoryArr;
   if (data) {
     categoryArr = data.categories;
